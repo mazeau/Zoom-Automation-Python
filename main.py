26e21732ef0fd0e30a98fa388b3e29caaf62371c
Emily Mazeau <mazeau.e@husky.neu.edu>
2020-10-22 19:09:21 -0400

    run automatically using chrontab
    removed the need for the csv file
    started to add additional checks
    fixed issue with finding center point on retina screens

--- main.py
+++ main.py
@@ -3,63 +3,80 @@ import pyautogui
 import time
 import pandas as pd
 from datetime import datetime
-import cv2
+import sys
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+def check_video_status():
+    if pyautogui.locateOnScreen(os.path.join(path, 'stop_video.png'), grayscale=True, confidence=0.9):
+        stop_video_btn = pyautogui.locateCenterOnScreen('stop_video.png', grayscale=True, confidence=0.9)
+        pyautogui.moveTo(stop_video_btn)
+        pyautogui.click()
+        print(f'Video turned off at {datetime.now()}')
+
+    if pyautogui.locateOnScreen(os.join.path(path, 'mute.png'), grayscale=True, confidence=0.9):
+        mute_btn = pyautogui.locateCenterOnScreen('mute.png', grayscale=True, confidence=0.9)
+        pyautogui.moveTo(mute_btn)
+        pyautogui.click()
+        print(f'Audio turned off at {datetime.now()}')
+
+
+def check_sucessful():
+    if pyautogui.locateOnScreen(os.join.path(path, 'leave_btn.png'), grayscale=True, confidence=0.9):
+        return True
+    elif pyautogui.locateOnScreen(os.join.path(path, 'waiting_for_host.png'), grayscale=True, confidence=0.9):
+        exit_btn = pyautogui.locateCenterOnScreen('exit.png', grayscale=True, confidence=0.9)
+        pyautogui.moveTo(exit_btn)
+        pyautogui.click()
+        time.sleep(60)
+        return False
+    elif pyautogui.locateOnScreen(os.join.path(path, 'unable_to_join.png'), grayscale=True, confidence=0.9):
+        okay_btn = pyautogui.locateCenterOnScreen('okay.png', grayscale=True, confidence=0.9)
+        pyautogui.moveTo(okay_btn)
+        pyautogui.click()
+        raise Exception("Unable to join meeting!")
+    else:
+        return False
+
 
 def sign_in(meetingid, pswd):
-    #Opens up the zoom app
+    # Opens up the zoom app
     subprocess.call(["/usr/bin/open", "/Applications/zoom.us.app"])
 
-    time.sleep(10)
+    time.sleep(5)
 
-    #clicks the join button
-    x,y = pyautogui.locateCenterOnScreen('join_button.png', grayscale=True)
-    join_btn = x*.5, y*.5  # don't know why it is off by a factor of 2
+    # clicks the join button
+    join_btn = pyautogui.locateCenterOnScreen(os.path.join(path, 'join_button.png'), grayscale=True, confidence=0.9)
+    # fixed retina doubling issue, see https://github.com/asweigart/pyautogui/issues/33
     pyautogui.moveTo(join_btn)
     pyautogui.click()
 
     # Type the meeting ID
-    # x,y =  pyautogui.locateCenterOnScreen('meeting_id_button.png', grayscale=True)
-    # meeting_id_btn = x*.5, y*.5
-    # pyautogui.moveTo(meeting_id_btn)
-    # pyautogui.click()
-    pyautogui.write(meetingid)
+    pyautogui.write(str(meetingid))
 
     # Disables both the camera and the mic
-    # x,y = pyautogui.locateAllOnScreen('media_btn.png', grayscale=True)
-    # media_btn = x*.5, y*.5
+    # media_btn = pyautogui.locateAllOnScreen('media_btn.png', grayscale=True, confidence=0.9)
     # for btn in media_btn:
-    #     pyautogui.moveTo(btn)
+    #     center = pyautogui.center(btn)
+    #     pyautogui.moveTo(center)
     #     pyautogui.click()
-    #     time.sleep(2)
+    #     time.sleep(1)
 
     # Hits the join button
     pyautogui.press('enter')
-    # x,y = pyautogui.locateCenterOnScreen('join_btn.png', grayscale=True)
-    # join_btn = x*.5, y*.5
-    # pyautogui.moveTo(join_btn)
-    # pyautogui.click()
+    time.sleep(1)
 
-    time.sleep(2)
-    #Types the password and hits enter
-    # x,y = pyautogui.locateCenterOnScreen('meeting_pswd.png', grayscale=True)
-    # meeting_pswd_btn = x*.5, y*.5
-    # pyautogui.moveTo(meeting_pswd_btn)
-    # pyautogui.click()
+    # Types the password and hits enter
     pyautogui.write(pswd)
     pyautogui.press('enter')
+    time.sleep(10)
 
-# Reading the file
-df = pd.read_csv('timings.csv')
 
-while True:
-    # checking of the current time exists in our csv file
-    now = datetime.now().strftime("%H:%M")
-    if now in str(df['timings']):
+if __name__ == "__main__":
+    m_id = int(sys.argv[1])
+    m_pswd = str(sys.argv[2])
 
-       row = df.loc[df['timings'] == now]
-       m_id = str(row.iloc[0,1])
-       m_pswd = str(row.iloc[0,2])
-
-       sign_in(m_id, m_pswd)
-       time.sleep(40)
-       print('signed in')
+    sign_in(m_id, m_pswd)
+    if check_sucessful() is True:
+        print(f'Sucessfully signed in at {datetime.now()}')
